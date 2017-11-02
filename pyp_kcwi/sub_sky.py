0e364cc7dabfb0cb3b2f42c6405aada6d204a6b1
Zheng Cai <zcai10@ucsc.edu>
2017-11-02 16:13:12 -0700

    Update sub_sky.py

--- pyp_kcwi/sub_sky.py
+++ pyp_kcwi/sub_sky.py
@@ -1,16 +1,18 @@
 # This function is used to subtract sky median
 #
-# INPUT:     - sky_file
-#            - img_file
-#            - mask_limit    mask limit
-#            - cut_ch        cut channel number
+# INPUT:     - sky_cube
+#            - img_cube
+#            - mask_limit   mask limit (e.g., 10 means the pixel in the same position of each layer is masked 10 times throughout the datacube)
+#            - cut_ch       cut layer number (the first and last few layers are bad)
 #
 # INPUT EXAMPLE:
-#   median('kb171021_00083_icuber.fits', 'kb171021_00082_icuber.fits', 40, cut_ch=300)
+#   median('kb171021_00083_icuber.fits', 'kb171021_00082_icuber.fits', 8, cut_ch=300)
 #
 # MODIFICATION HISTORY:
 #   Written by: Qiong Li
+#   Modified by Zheng Cai
 #   2017-11-01 Initial version
+#   2017-11-02 Second version
 
 def median(sky_file, img_file, mask_limit, cut_ch=300):
     from astropy.io import fits
@@ -47,9 +49,6 @@ def median(sky_file, img_file, mask_limit, cut_ch=300):
             min_3 = np.median(s[need])-3*np.std(s[need])
             mask[(s>max_3) | (s<min_3) ]=1
 
-            ######### check convergence:
-            #print i, np.median(s[mask == 0])
-
         sky_value = np.median(s[mask == 0])
 
         sky[j,:,:]-=sky_value
@@ -66,8 +65,8 @@ def median(sky_file, img_file, mask_limit, cut_ch=300):
     #plt.text(1100,1300,'e.g. 1400 pixels are masked 1000 times(channels)')
     #plt.show()
     ################
-    mask_sum[mask_sum<mask_limit]=0
-    mask_sum[mask_sum>=mask_limit]=1
+    mask_sum[mask_sum<mask_limit]=0     
+    mask_sum[mask_sum>=mask_limit]=1   
 
     ################ check mask image:
     #face = plt.imshow(mask_sum, aspect='equal', interpolation='None',origin='lower',vmin=0,vmax=1.0,cmap = mpl.cm.gray)
@@ -95,7 +94,8 @@ def median(sky_file, img_file, mask_limit, cut_ch=300):
 #   psf('img.fits', dpx=8, dpy=10)
 #
 # MODIFICATION HISTORY:
-#   Written by: Qiong Li
+#   Written by: Qiong Li 
+#   Modified by Zheng Cai
 #   2017-11-01 Initial version
 
 def psf(img_file, dpx, dpy):
@@ -106,16 +106,22 @@ def psf(img_file, dpx, dpy):
     img, ih = fits.getdata(img_file, header=True)
 
     ### PSF reduced
-    img_median = np.zeros(img[1,:,:].shape)
-    for x in np.arange(0, len(img[1,:,1]), 1):
-        for y in np.arange(0, len(img[1,1,:]), 1):
-            img_median[x,y] = np.median(img[:,x,y])
-
+    #img_median = np.zeros(img[1,:,:].shape)
+    #for x in np.arange(0, len(img[1,:,1]), 1):
+    #    for y in np.arange(0, len(img[1,1,:]), 1):
+    #        img_median[x,y] = np.median(img[:,x,y])
+    img_median= np.median(img, axis=0)
+    
     # find peak location
-    peak = np.max(img_median)
-    loc = np.where(img_median == peak)
-    px=loc[1][0]  # add[0]: Error only integer scalar arrays can be converted to a scalar index
-    py=loc[0][0]
+    #peak = np.max(img_median)
+    #loc = np.where(img_median == peak)
+    #px=loc[1][0]  # add[0]: Error only integer scalar arrays can be converted to a scalar index
+    #py=loc[0][0]
+    
+    loc = np.where(img_median > 0.95*np.amax(img_median))    
+    px= int(round(np.mean(indices[0])))
+    py= int(round(np.mean(indices[1])))
+    
 
     #################### test peak
     #face = plt.imshow(img_median, aspect='equal', interpolation='None',origin='lower',cmap = mpl.cm.gray)
