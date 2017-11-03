1c0c68c4d7012ae29000173935a4ba39a1f49177
李琼 <qiong@3c273.attlocal.net>
2017-11-03 01:12:29 -0700

    sub_sky.py

--- pyp_kcwi/sub_sky.py
+++ pyp_kcwi/sub_sky.py
@@ -1,25 +1,31 @@
-# This function is used to subtract sky median
-#
-# INPUT:     - sky_file
-#            - img_file
-#            - mask_limit    mask limit
-#            - cut_ch        cut channel number
-#
-# INPUT EXAMPLE:
-#   median('kb171021_00083_icuber.fits', 'kb171021_00082_icuber.fits', 40, cut_ch=300)
-#
-# MODIFICATION HISTORY:
-#   Written by: Qiong Li
-#   2017-11-01 Initial version
+""" Functions to subtract sky:
+"""
 
 def median(sky_file, img_file, mask_limit, cut_ch=300):
+    """ This function is used to subtract sky median
+
+    Parameters
+    ----------
+    sky_file
+    img_file
+    mask_limit : mask limit
+    cut_ch     : cut bad channel number
+
+    Eaxmple:
+    median('kb171021_00083_icuber.fits', 'kb171021_00082_icuber.fits', 40, cut_ch=300)
+
+    Returns
+    -------
+    mask.fits  : masked image (mask the bad pixels and qso)
+    img.fits   : sky median subtracted image
+    """
     from astropy.io import fits
     import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib as mpl
+    from astropy.stats import sigma_clip
     sky_input, sh = fits.getdata(sky_file, header=True)
     img_input, ih = fits.getdata(img_file, header=True)
 
+    # goodwave layers, default clip 300 layers at the beginning and the end
     img=img_input[cut_ch:(len(img_input[:,1,1])-cut_ch),:,:]
     sky=sky_input[cut_ch:(len(img_input[:,1,1])-cut_ch),:,:]
 
@@ -29,53 +35,21 @@ def median(sky_file, img_file, mask_limit, cut_ch=300):
 
     ## sky[w,x,y]; for every wavelength: len(sky[:,1,1])
     for j in np.arange(0, len(sky[:,1,1]), 1):
-
-        # sky image for each channel
-        s=sky[j,:,:]
-
-        # define mask image for each channel
-        mask = np.zeros(sky[1,:,:].shape)
-
-    # begin 10 loops
-        for i in np.arange(0, 10, 1):
-
-            # select: s>0(remove egde) & mask<1(loop new sky)
-            need=(mask<1)&(s!=0)
-
-            # mask out of 'mean + [-3, 3] sigma' as 1, sky image: mask == 0
-            max_3 = np.median(s[need])+3*np.std(s[need])
-            min_3 = np.median(s[need])-3*np.std(s[need])
-            mask[(s>max_3) | (s<min_3) ]=1
-
-            ######### check convergence:
-            #print i, np.median(s[mask == 0])
-
-        sky_value = np.median(s[mask == 0])
-
+        # iterated 10 times, rejecting points by > 3 sigma
+        filtered_data = sigma_clip(sky[j,:,:], sigma=3, iters=10)
+        sky_value = np.median(filtered_data.data[filtered_data.mask == False])
+        # subtract the sky median value
         sky[j,:,:]-=sky_value
         img[j,:,:]-=sky_value
+        # record the masked counts
+        mask = np.zeros(sky[1,:,:].shape)
+        mask[filtered_data.mask == True] = 1
         mask_sum+=mask
 
-
-    ################ check and select limit:
-    # transfer to 1D
-    #mask_sum_1D=mask_sum[mask_sum>(mask_sum-1)]
-    #hist=plt.hist(mask_sum_1D,np.arange(-10, 3000,1),histtype='step')
-    #plt.xlabel('counts of mask')
-    #plt.ylabel('counts of pixel')
-    #plt.text(1100,1300,'e.g. 1400 pixels are masked 1000 times(channels)')
-    #plt.show()
-    ################
+    # use mask_limit to avoid masking the good pixels (noise)
     mask_sum[mask_sum<mask_limit]=0
     mask_sum[mask_sum>=mask_limit]=1
 
-    ################ check mask image:
-    #face = plt.imshow(mask_sum, aspect='equal', interpolation='None',origin='lower',vmin=0,vmax=1.0,cmap = mpl.cm.gray)
-    #cbar = plt.colorbar(face)
-    #plt.text(55,95,'mask')
-    #plt.text(55,-2,'sky')
-    #plt.show()
-    ################
     fits.writeto('mask.fits',mask_sum)
     fits.writeto('img.fits',img, ih)
 
@@ -85,55 +59,53 @@ def median(sky_file, img_file, mask_limit, cut_ch=300):
 
 
 
-# This function is used to subtract PSF for bright point source
-#
-# INPUT:     - img_file      default 'img.fits'
-#            - dpx           cut PSF region in x
-#            - dpy           cut PSF region in y
-#
-# INPUT EXAMPLE:
-#   psf('img.fits', dpx=8, dpy=10)
-#
-# MODIFICATION HISTORY:
-#   Written by: Qiong Li
-#   2017-11-01 Initial version
 
-def psf(img_file, dpx, dpy):
+def psf(img_file, dpx, dpy, scale):
+    """ This function is used to subtract PSF for bright point source
+
+    Parameters
+    ----------
+    img_file  :   default 'img.fits'
+    dpx       :   cut PSF region in x
+    dpy       :   cut PSF region in y
+    scale     :   sclae method: 'peak', 'p', 'integrated', 'i'
+
+    Eaxmple:
+    psf('img.fits', dpx=8, dpy=10, scale='integrated')
+
+    Returns
+    -------
+    img_psf.fits  : psf subtracted image
+    """
     from astropy.io import fits
     import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib as mpl
     img, ih = fits.getdata(img_file, header=True)
 
     ### PSF reduced
-    img_median = np.zeros(img[1,:,:].shape)
-    for x in np.arange(0, len(img[1,:,1]), 1):
-        for y in np.arange(0, len(img[1,1,:]), 1):
-            img_median[x,y] = np.median(img[:,x,y])
+    img_median= np.median(img, axis=0)
 
     # find peak location
-    peak = np.max(img_median)
-    loc = np.where(img_median == peak)
-    px=loc[1][0]  # add[0]: Error only integer scalar arrays can be converted to a scalar index
-    py=loc[0][0]
-
-    #################### test peak
-    #face = plt.imshow(img_median, aspect='equal', interpolation='None',origin='lower',cmap = mpl.cm.gray)
-    #cbar = plt.colorbar(face)
-    #plt.scatter(px, py,color='r', marker='+', s=60,linewidths=1.5)
-    #plt.show()
-    ####################
+    loc = np.where(img_median > 0.9*np.amax(img_median))
+    px= int(round(np.mean(loc[1])))
+    py= int(round(np.mean(loc[0])))
 
     a = np.zeros(img[:,1,1].shape)
     psf = np.zeros(img.shape)
 
-    # scale to each channel
-    peak_value = np.max(img_median)
-    a=img[:,py,px]/peak_value
-
+    ## scale to each channel
+    if scale not in ['peak', 'p', 'integrated', 'i']:
+        raise IOError('Need input the scale method! scale = peak / integrated')
+    # peak value to scale
+    if scale in ['peak', 'p']:
+        peak_value = np.max(img_median)
+        a=img[:,py,px]/peak_value
 
     # cut the region
     for j in np.arange(0, len(img[:,1,1]), 1):
+        # integrated to scale
+        if scale in ['integrated', 'i']:
+            integrated_value = np.sum(img[j,(py-dpy):(py+dpy),(px-dpx):(px+dpx)])
+            a[j]=integrated_value/np.sum(img_median[(py-dpy):(py+dpy),(px-dpx):(px+dpx)])
         psf[j,(py-dpy):(py+dpy),(px-dpx):(px+dpx)]=img_median[(py-dpy):(py+dpy),(px-dpx):(px+dpx)]*a[j]
 
     # reduce psf
